0f13f7b0d21dcc83742bb9fd9956ac2d5524fd6b
danyel duckman <danyel.duckman@gmail.com>
2023-04-12 12:23:56 +0200

    quete 04

--- Movies.java
+++ Movies.java
@@ -1,6 +1,17 @@
-public class Movie {
-    public static void main(String[] args) {
+public class Movies {
+
+public static void main(String[] args) {
+
         String[] movies = {"Les Aventuriers de l'Arche", "Le Temple de l'Arche", "La Dernière Croisade"};
-        String[][] actors = {""} 
-    }
-}
+
+        String[][] actors = {{"Harrison Ford", "Karen Allen", "Paul Freeman"},
+        {"Harrison Ford", "Kate Capshaw", "Ke Hury Quan"},
+        {"Harrison Ford", "Sean Connery", "Denholm Elliott"}}; 
+    
+
+        for (int i = 0; i < movies.length; i++) {
+            System.out.println("Dans le film " + movies[i] + ", les principaux acteurs sont : " + actors[i][0] + ", "
+            + actors[i][1] + ", " + actors[i][2]);
+        }
+    };
+};
